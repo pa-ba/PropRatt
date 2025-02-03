dff16fe406bf2aa9dc63fa47c4f051a9fe881f65
fridge7809 <127931228+fridge7809@users.noreply.github.com>
2025-02-03 13:47:26 +0100

    first draft LTL syntax

--- PropRatt/src/Main.hs
+++ PropRatt/src/Main.hs
@@ -4,29 +4,9 @@ import Test.QuickCheck
 import PropRatt.AsyncRat
 import PropRatt.Generators
 import PropRatt.Properties
+import PropRatt.LTL (Pred (..), evaluateLTL)
 
 
 main :: IO ()
 main = do
-    -- testing switch and tail equality
-    startSignal <- generate (arbitrary :: Gen (Sig Int))
-    laterSignal <- generate (arbitrary :: Gen (Sig Int))
-    let later = getLater laterSignal
-    let switched = aRatSwitch startSignal later
-    let switchedCorrectly = prop_eventual_equality startSignal laterSignal
-    --putStrLn (show startSignal)
-    --putStrLn (show laterSignal)
-    --putStrLn (show switched)
-    putStrLn (show switchedCorrectly)
-
-    -- testing zip and stutter
-    xs <- generate (arbitrary :: Gen (Sig Int))
-    ys <- generate (arbitrary :: Gen (Sig Int))
-    let stutter = stuttering xs ys
-    let zipped = aRatZip xs ys
-    let isStuttering = prop_is_stuttering xs ys
-    --putStrLn (show xs)
-    --putStrLn (show ys)
-    --putStrLn (show zipped)
-    --putStrLn (show stutter)
-    putStrLn (show isStuttering)
+    print (evaluateLTL (Eventually (Implies (Atom odd) (NextTime (Atom (== 4))))) [2,2,2,3,4])
--- PropRatt/src/PropRatt/LTL.hs
+++ PropRatt/src/PropRatt/LTL.hs
@@ -1,41 +1,47 @@
-module PropRatt.PropRatt.LTL where
+{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module PropRatt.LTL (
+    Pred (..),
+    evaluateLTL
+)where
 
 
-data Predicate a where
-    Tautology :: Predicate a
-    Contradiction :: Predicate a
-    Atom :: (a -> Bool) -> Predicate a
-    Not :: Predicate a -> Predicate a
-    And :: Predicate a -> Predicate a -> Predicate a
-    Or :: Predicate a -> Predicate a -> Predicate a
-    Until :: Predicate a -> Predicate a -> Predicate a
-    WUntil :: Predicate a -> Predicate a -> Predicate a
-    NextTime :: Predicate a -> Predicate a
-    Implies :: Predicate a -> Predicate a -> Predicate a
-    Always :: Predicate a -> Predicate a
-    Eventually :: Predicate a -> Predicate a
-    After :: Int -> Predicate a -> Predicate a
-    Release :: Predicate a -> Predicate a -> Predicate a
+data Pred a where
+    Tautology :: Pred a
+    Contradiction :: Pred a
+    Atom :: (a -> Bool) -> Pred a
+    Not :: Pred a -> Pred a
+    And :: Pred a -> Pred a -> Pred a
+    Or :: Pred a -> Pred a -> Pred a
+    Until :: Pred a -> Pred a -> Pred a
+    WUntil :: Pred a -> Pred a -> Pred a
+    NextTime :: Pred a -> Pred a
+    Implies :: Pred a -> Pred a -> Pred a
+    Always :: Pred a -> Pred a
+    Eventually :: Pred a -> Pred a
+    After :: Int -> Pred a -> Pred a
+    Release :: Pred a -> Pred a -> Pred a
 
 
 
-evaluateLTL :: Int -> Predicate a -> [a] -> Bool
-evaluateLTL amountOfPredCap formulae ls@(x:xs) = amountOfPredCap <= 0
-     || case formulae of
-         Tautology       -> True
-         Contradiction   -> False
-         Atom phi        -> phi x
-         Not phi         -> not (evaluate phi ls)
-         And phi psi     -> evaluate phi ls && evaluate psi ls
-         Or phi psi      -> evaluate phi ls || evaluate psi ls
-         Until phi psi   -> evaluate psi ls || (evaluate phi ls && evaluateNext (phi `Until` psi) xs) -- U (Until)
-         WUntil phi psi  -> ? -- W (Weak-Until)
-         NextTime phi    -> evaluateNext phi xs -- X (Next)
-         Implies phi psi -> not (evaluate phi ls && not (evaluate psi ls)) -- (phi: True, psi: False) = False (else True)
-         Always phi      -> evaluate phi ls && evaluateNext phi xs -- G (In all future states)
-         Eventually phi  -> evaluate phi ls || evaluateNext phi xs -- F (in some future timestep)
-         After i phi     -> evaluateNext phi xs -- Wrong
-         Release phi psi -> not (evaluate ((not phi) `Until` (not psi)) ls) -- R (Release)
-     where
-         evaluateNext = evaluateLTL (amountOfPredCap - 1)
-         evaluate = evaluateLTL amountOfPredCap
+evaluateLTL' :: Int -> Pred a -> [a] -> Bool
+evaluateLTL' amountOfPredCap formulae ls@(x:xs) = amountOfPredCap <= 0
+    || case formulae of
+        Tautology       -> True
+        Contradiction   -> False
+        Atom phi        -> phi x
+        Not phi         -> not (evaluate phi ls)
+        And phi psi     -> evaluate phi ls && evaluate psi ls
+        Or phi psi      -> evaluate phi ls || evaluate psi ls
+        Until phi psi   -> evaluate psi ls || (evaluate phi ls && evaluateNext (phi `Until` psi) xs) -- U (Until)
+        NextTime phi    -> evaluateNext phi xs -- X (Next)
+        Implies phi psi -> not (evaluate phi ls && not (evaluate psi ls)) -- (phi: True, psi: False) = False (else True)
+        Always phi      -> evaluate phi ls && evaluateNext (Always phi) xs -- G (In all future states)
+        Eventually phi  -> evaluate phi ls || evaluateNext (Eventually phi) xs -- F (in some future timestep)
+    where
+        evaluateNext = evaluateLTL' (amountOfPredCap - 1)
+        evaluate = evaluateLTL' amountOfPredCap
+
+evaluateLTL :: Pred a -> [a] -> Bool
+evaluateLTL = evaluateLTL' 20

d3fd4b65712239381ffb50cf788ed0514106bc2f
fridge7809 <127931228+fridge7809@users.noreply.github.com>
2025-02-05 14:14:10 +0100

    model traffic light system

--- PropRatt/src/Main.hs
+++ PropRatt/src/Main.hs
@@ -1,12 +1,71 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 module Main (main) where
-import Test.QuickCheck
-import PropRatt.AsyncRat
-import PropRatt.Generators
-import PropRatt.Properties
 import PropRatt.LTL (Pred (..), evaluateLTL)
-
 
 main :: IO ()
 main = do
-    print (evaluateLTL (Eventually (Implies (Atom odd) (NextTime (Atom (== 4))))) [2,2,2,3,4])
+    -- print (evaluateLTL (Eventually (Implies (Atom odd) (Next (Atom (== 4))))) [2,2,2,3,4])
+    -- print (evaluateLTL (Eventually (Atom odd)) [2,2,2,2,2,2])
+    -- print (evaluateLTL (Implies
+    --                        (And (Eventually (Atom (odd))) (Always (Atom (>= 20)))) --false
+    --                        (Until (Atom (>= 20)) (Atom (== 50)))) -- false
+    --                    [0,0,0,23,19])
+    {- print (evaluateLTL (Or (
+                            Eventually (
+                                Implies (Atom id) (Always (Atom id))
+                            ))
+                            (
+                                Until (Not (Atom id)) (Atom id)
+                            ))
+                            [False,True,True]) -- Pre-order traversal of Fig 3.1, formula: (F (p -> G r) v (neg q U p)) -}
+
+    -- Atoms:
+    -- p = Red light is activated
+    -- q = Yellow light is activated
+    -- r = Green light is activated
+
+    -- States S = {s1,s2,s3,s4}
+    -- L(s1) = p
+    -- L(s2) = q
+    -- L(s3) = r
+    -- L(s4) = q
+
+    -- Transitions
+    -- s1 -> s2
+    -- s2 -> s3
+    -- s3 -> s4
+    -- s4 -> s1
+
+    let goodList = ["Green", "Yellow", "Red", "Yellow", "Green", "Yellow"]
+    let badList = ["Yellow", "Yellow", "Red", "Green", "Green", "Yellow"]
+    let badList2 = ["Purple", "Purple", "Purple", "Purple", "Purple"]
+
+    -- Formulae
+    -- 1. A least one light is activated at all times (G (p v q v r))
+    print (evaluateLTL (Always (Or (Or (Atom (== "Green")) (Atom (== "Yellow"))) (Atom (== "Red")))) goodList)
+    -- 2. A light can never have two lights activated at once (requires concurrency, can do this with a list..)
+    -- 3. Red light is always followed by a yellow light (G (p -> X q))
+    print (evaluateLTL (Always (Implies (Atom (== "Red")) (Next (Atom (== "Yellow"))))) goodList)
+    -- 4. Yellow light is always followed by either a green or red light (G (q -> X (p v r)))
+    print (evaluateLTL (Always (Implies (Atom (== "Yellow")) (Next (Or (Atom (== "Red")) (Atom (== "Green")))))) goodList)
+    -- 5. Green light is always followed by a yellow light (G (r -> X q))
+    print (evaluateLTL (Always (Implies (Atom (== "Green")) (Next (Atom (== "Yellow"))))) goodList)
+    -- 6. A light eventuall turns green (G F p)
+    print (evaluateLTL (Always (Eventually (Atom (== "Green")))) goodList)
+    -- 7. A light eventuall turns yellow (G F q)
+    print (evaluateLTL (Always (Eventually (Atom (== "Yellow")))) goodList)
+    -- 8. A light eventuall turns red (G F r)
+    print (evaluateLTL (Always (Eventually (Atom (== "Red")))) goodList)
+
+    print (evaluateLTL (Always (Or (Or (Atom (== "Green")) (Atom (== "Yellow"))) (Atom (== "Red")))) badList) -- 1
+    print (evaluateLTL (Always (Implies (Atom (== "Red")) (Next (Atom (== "Yellow"))))) badList) -- 3
+    print (evaluateLTL (Always (Implies (Atom (== "Yellow")) (Next (Or (Atom (== "Red")) (Atom (== "Green")))))) badList) -- 4
+    print (evaluateLTL (Always (Implies (Atom (== "Green")) (Next (Atom (== "Yellow"))))) badList) -- 5
+    print (evaluateLTL (Always (Eventually (Atom (== "Green")))) badList2) -- 6
+    print (evaluateLTL (Always (Eventually (Atom (== "Yellow")))) badList2) -- 7
+    print (evaluateLTL (Always (Eventually (Atom (== "Red")))) badList2) -- 8
+
+
+
+
+
--- PropRatt/src/PropRatt/LTL.hs
+++ PropRatt/src/PropRatt/LTL.hs
@@ -3,9 +3,13 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module PropRatt.LTL (
     Pred (..),
-    evaluateLTL
-)where
+    evaluateLTL,
+    evaluateLTLSig
+) where
 
+import AsyncRattus.Signal
+import AsyncRattus.InternalPrimitives
+import qualified Data.IntSet as IntSet
 
 data Pred a where
     Tautology :: Pred a
@@ -15,15 +19,12 @@ data Pred a where
     And :: Pred a -> Pred a -> Pred a
     Or :: Pred a -> Pred a -> Pred a
     Until :: Pred a -> Pred a -> Pred a
-    WUntil :: Pred a -> Pred a -> Pred a
-    NextTime :: Pred a -> Pred a
+    Next :: Pred a -> Pred a
     Implies :: Pred a -> Pred a -> Pred a
     Always :: Pred a -> Pred a
     Eventually :: Pred a -> Pred a
     After :: Int -> Pred a -> Pred a
     Release :: Pred a -> Pred a -> Pred a
-
-
 
 evaluateLTL' :: Int -> Pred a -> [a] -> Bool
 evaluateLTL' amountOfPredCap formulae ls@(x:xs) = amountOfPredCap <= 0
@@ -35,13 +36,39 @@ evaluateLTL' amountOfPredCap formulae ls@(x:xs) = amountOfPredCap <= 0
         And phi psi     -> evaluate phi ls && evaluate psi ls
         Or phi psi      -> evaluate phi ls || evaluate psi ls
         Until phi psi   -> evaluate psi ls || (evaluate phi ls && evaluateNext (phi `Until` psi) xs) -- U (Until)
-        NextTime phi    -> evaluateNext phi xs -- X (Next)
+        Next phi        -> evaluateNext phi xs -- X (Next)
         Implies phi psi -> not (evaluate phi ls && not (evaluate psi ls)) -- (phi: True, psi: False) = False (else True)
-        Always phi      -> evaluate phi ls && evaluateNext (Always phi) xs -- G (In all future states)
-        Eventually phi  -> evaluate phi ls || evaluateNext (Eventually phi) xs -- F (in some future timestep)
+        Always phi      -> evaluate phi ls && evaluateNext (Always phi) xs -- G (Globally)
+        Eventually phi  -> not (amountOfPredCap == 1 && not (evaluate phi ls)) && (evaluate phi ls || evaluateNext (Eventually phi) xs) -- F (Later)
+        Release phi psi -> (evaluate psi ls && evaluate phi ls) || (evaluate psi ls && evaluateNext (phi `Until` psi) xs) -- R (Release)
+        After n phi     -> if n <= 0 then evaluate phi ls else evaluateNext (After (n - 1) phi) ls
     where
         evaluateNext = evaluateLTL' (amountOfPredCap - 1)
         evaluate = evaluateLTL' amountOfPredCap
 
+evaluateLTLSig' :: Int -> Pred a -> Sig a -> Bool
+evaluateLTLSig' amountOfPredCap formulae ls@(x ::: Delay cl f) = amountOfPredCap <= 0
+    || case formulae of
+        Tautology       -> True
+        Contradiction   -> False
+        Atom phi        -> phi x
+        Not phi         -> not (evaluate phi ls)
+        And phi psi     -> evaluate phi ls && evaluate psi ls
+        Or phi psi      -> evaluate phi ls || evaluate psi ls
+        Until phi psi   -> evaluate psi ls || (evaluate phi ls && evaluateNext (phi `Until` psi) (f (InputValue (smallest cl) ()))) -- U (Until)
+        Next phi        -> evaluateNext phi (f (InputValue (smallest cl) ())) -- X (Next)
+        Implies phi psi -> not (evaluate phi ls && not (evaluate psi ls)) -- (phi: True, psi: False) = False (else True)
+        Always phi      -> evaluate phi ls && evaluateNext (Always phi) (f (InputValue (smallest cl) ())) -- G (Globally)
+        Eventually phi  -> not (amountOfPredCap == 1 && not (evaluate phi ls)) && (evaluate phi ls || evaluateNext (Eventually phi) (f (InputValue (smallest cl) ()))) -- F (Later)
+        Release phi psi -> (evaluate psi ls && evaluate phi ls) || (evaluate psi ls && evaluateNext (phi `Until` psi) (f (InputValue (smallest cl) ()))) -- R (Release)
+        After n phi     -> if n <= 0 then evaluate phi ls else evaluateNext (After (n - 1) phi) ls
+    where
+        evaluateNext = evaluateLTLSig' (amountOfPredCap - 1)
+        evaluate = evaluateLTLSig' amountOfPredCap
+        smallest = IntSet.findMin
+
 evaluateLTL :: Pred a -> [a] -> Bool
-evaluateLTL = evaluateLTL' 20
+evaluateLTL = evaluateLTL' 5
+
+evaluateLTLSig :: Pred a -> Sig a -> Bool
+evaluateLTLSig = evaluateLTLSig' 10

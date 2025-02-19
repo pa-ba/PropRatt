68808b5eab23662aaaf66449f280b7c3be2eded4
fridge7809 <127931228+fridge7809@users.noreply.github.com>
2025-02-19 14:24:20 +0100

    implement hlist

--- PropRatt/src/PropRatt/AsyncRat.hs
+++ PropRatt/src/PropRatt/AsyncRat.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
+{-# LANGUAGE DataKinds #-}
 
 
 
@@ -15,7 +16,6 @@ module PropRatt.AsyncRat (
     aRatParallel,
     prepend,
     singleton',
-    flattenToSignal,
     flattenToSignal'
 ) where
 
@@ -24,9 +24,9 @@ import AsyncRattus.Signal hiding (mkSig)
 import AsyncRattus.Strict
 import AsyncRattus.InternalPrimitives
 import Prelude hiding (const, filter, getLine, map, null, putStrLn, zip, zipWith)
-import PropRatt.Value (Value(..))
+import PropRatt.Value (Value(..),HList(..),(%:),mapH)
 
-data NonEmptyList a = NonEmptyList !a !(List a)
+-- data NonEmptyList a = NonEmptyList !a !(HList a)
 
 aRatZip :: Sig Int -> Sig Int -> Sig (Int :* Int)
 aRatZip a b = zip a b
@@ -37,26 +37,26 @@ aRatSwitch a o = switch a o
 aRatParallel :: Sig a -> Sig b -> Sig (Maybe' a :* Maybe' b)
 aRatParallel a b = parallel a b
 
-singleton' :: Sig a -> Sig (List (Value a))
-singleton' xs = map (box (\x -> Current (Just' x) x :! Nil)) xs
+singleton' :: Sig a -> Sig (HList '[Value a])
+singleton' xs = map (box (\x -> Current (Just' x) x %: HNil)) xs
 
-prepend :: (Stable a) => Sig a -> Sig (List (Value a)) -> Sig (List (Value a))
+prepend :: (Stable a) => Sig a -> Sig (HList '[Value a]) -> Sig (HList '[Value a])
 prepend (x ::: xs) (y ::: ys) =
-   (Current (Just' x) x :! y) ::: prependAwait x xs y ys
+   (Current (Just' x) x %: y) ::: prependAwait x xs y ys
 
-prependAwait :: (Stable a) => a -> O (Sig a) -> List (Value a) -> O (Sig (List (Value a))) -> O (Sig (List (Value a)))
+prependAwait :: (Stable a) => a -> O (Sig a) -> HList '[Value a] -> O (Sig (HList '[Value a])) -> O (Sig (HList '[Value a]))
 prependAwait x xs y ys  = delay (
   case select xs ys of
-     Fst (x' ::: xs')   ys'         -> (Current (Just' x') x' :! map' (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait x' xs' y ys'
-     Snd xs' (y' ::: ys')           -> (Current Nothing' x :! y') ::: prependAwait x xs' y' ys'
-     Both (x' ::: xs') (y' ::: ys') -> (Current (Just' x') x' :! y')  ::: prependAwait x' xs' y' ys')
+     Fst (x' ::: xs')   ys'         -> (Current (Just' x') x' %: mapH (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait x' xs' y ys'
+     Snd xs' (y' ::: ys')           -> (Current Nothing' x %: y') ::: prependAwait x xs' y' ys'
+     Both (x' ::: xs') (y' ::: ys') -> (Current (Just' x') x' %: y')  ::: prependAwait x' xs' y' ys')
 
 {-# ANN flattenToSignal' AllowRecursion #-}
-flattenToSignal' :: (Stable a) => List (Sig a) -> Sig (List (Value a))
-flattenToSignal' Nil = undefined
-flattenToSignal' (h :! Nil) = singleton' h
-flattenToSignal' (h :! t)   = prepend h (flattenToSignal' t)
+flattenToSignal' :: (Stable a) => HList '[Sig a] -> Sig (HList '[Value a])
+flattenToSignal' HNil = undefined
+flattenToSignal' (HCons h HNil) = singleton' h
+flattenToSignal' (HCons h t)   = prepend h (flattenToSignal' t)
 
-flattenToSignal :: (Stable a) => NonEmptyList (Sig a) -> Sig (List (Value a))
-flattenToSignal (NonEmptyList h Nil) = singleton' h
-flattenToSignal (NonEmptyList h t)   = prepend h (flattenToSignal' t)
+-- flattenToSignal :: (Stable a) => NonEmptyList (Sig a) -> Sig (HList (Value a))
+-- flattenToSignal (NonEmptyList h Nil) = singleton' h
+-- flattenToSignal (NonEmptyList h t)   = prepend h (flattenToSignal' t)
--- PropRatt/src/PropRatt/Value.hs
+++ PropRatt/src/PropRatt/Value.hs
@@ -1,10 +1,42 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
-module PropRatt.Value (Value(..),(?=)) where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module PropRatt.Value where
 import AsyncRattus.Strict
 import AsyncRattus.Signal hiding (current)
 import PropRatt.Utilities
+import Data.Kind ( Type )
+
+data HList :: [Type] -> Type where
+  HNil :: HList '[]
+  HCons :: !x -> !(HList xs) -> HList (x ': xs)
+  
+instance Show (HList '[]) where
+  show HNil = "HNil"
+
+list :: HList [Integer, String]
+list = HCons 5 (HCons "Hej" (HNil))
+
+infixr 5 %:
+(%:) :: x -> HList xs -> HList (x ': xs)
+(%:) = HCons
+
+instance (Show x, (Show (HList xs))) => Show (HList (x ': xs)) where
+  show (HCons x xs) = show x ++ " %: " ++ show xs 
+
+class Apply f a b where
+  apply :: f -> a -> b
+
+class MapH f xs ys where
+  mapH :: f -> HList xs -> HList ys
+  
+instance (Apply f x y, MapH f xs ys) => MapH f (x ': xs) (y ': ys) where
+  mapH f (HCons x xs) = apply f x %: mapH f xs
 
 data Value a where
   Current :: !(Maybe' a) -> !a -> Value a

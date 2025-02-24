245581b0daa8fec7dda86a6ed9944fb42dc2b951
fridge7809 <127931228+fridge7809@users.noreply.github.com>
2025-02-24 14:18:40 +0100

    fix illegal type synonym application

--- PropRatt/src/PropRatt/AsyncRat.hs
+++ PropRatt/src/PropRatt/AsyncRat.hs
@@ -13,7 +13,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-
 -- AsyncRattus code goes here. 
 -- The code is type checked by the AsyncRattus compiler plugin.
 
@@ -61,89 +60,36 @@ aRatSwitch a o = switch a o
 aRatParallel :: Sig a -> Sig b -> Sig (Maybe' a :* Maybe' b)
 aRatParallel a b = parallel a b
 
--- singleton' :: forall a xs. (MapH ToValue xs (Map xs)) => Sig a -> Sig (HList (Map (Value a ': xs)))
--- singleton' xs = map (box (\x -> mapH ToValue (x %: HNil))) xs
+singleton' :: Sig a -> Sig (HList (Map '[Value a]))
+singleton' xs = map (box (\p -> Current (Just' p) p %: HNil)) xs
 
-prepend :: (Stable a) => Sig a -> Sig (WrappedMap (Value a ': xs)) -> Sig (WrappedMap (Value a ': xs))
-prepend (x ::: xs) (y ::: ys) =
-   (Current (Just' x) x %: y) ::: prependAwait x xs y ys
+-- lacks maph constraint
+prepend :: (Stable a) => Sig a -> Sig (HList (Map (Value a ': xs))) -> Sig (HList (Map (Value a ': xs)))
+prepend q (y ::: ys) = do
+  -- make input sig into same type as acc :: Sig (HList (Map (Value a) xs))
+  let ((HCons o p) ::: xs) = singleton' q 
+  (o %: y) ::: prependAwait xs ys
 
--- prependAwait :: forall a xs. (MapH ToValue xs (Map xs), Stable a) => a -> O (Sig a) -> HList (Value a ': xs) -> O (Sig (HList (Value a ': xs))) -> O (Sig (HList (Map (Value a ': xs))))
--- prependAwait x xs y ys  = delay (
---   case select xs ys of
---      Fst (x' ::: xs')   ys'         -> (Current (Just' x') x' %: mapH (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait x' xs' y ys'
---      Snd xs' (y' ::: ys')           -> (Current Nothing' x %: y') ::: prependAwait x xs' y' ys'
---      Both (x' ::: xs') (y' ::: ys') -> (Current (Just' x') x' %: y')  ::: prependAwait x' xs' y' ys')
+-- lacks maph constraint
+-- lacks symbol function to map values to nothing
+prependAwait :: (Stable a) => O (Sig (HList (Value a ': xs))) -> O (Sig (HList( Map (Value a ': xs)))) -> O (Sig (HList (Map (Value a ': xs))))
+prependAwait xs ys = delay (
+  case select xs ys of
+     Fst (x' ::: xs')   ys'         -> (x' %: mapH (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait xs' ys'
+     Snd xs' (y' ::: ys')           -> mapH (\(Current _ x) -> Current Nothing' x) xs' %: y' ::: prependAwait xs' ys'
+     Both (x' ::: xs') (y' ::: ys') -> (x' %: y')  ::: prependAwait xs' ys')
 
--- toValue :: a -> Value a
--- toValue a = Currrent (Just' a) a
-
-data Pretty = Pretty
-instance Show x => Apply Pretty x String where
-  apply _ x = show x
-
-data Concat = Concat
-instance Apply Concat String (String -> String) where
-  apply _ x = \y -> x ++ ", " ++ y
-
-type family ConstMap (t :: *) (xs :: [*]) :: [*] where
-  ConstMap _      '[]  = '[]
-  ConstMap t (x ': xs) = t ': (ConstMap t xs)
-
-
-prettyHList :: forall xs.
-               ( MapH   Pretty xs     (ConstMap String xs)
-               , FoldrH Concat String (ConstMap String xs)
-               ) => HList xs -> String
-prettyHList hlist = "[" ++ (foldrH Concat "" $ mapH @_ @_ @(ConstMap String xs) Pretty hlist) ++ "]"
-
--- Defunctionalization notes
--- https://www.geeksforgeeks.org/what-is-defunctionalization/
-
-
--- what
--- Defunctionalization is a compile-time conversion which removes higher-order functions by replacing the higher-order functions with a single first-order apply function. 
--- idea: convert higher order functions (a -> b -> c) into its first order representation
--- quick recap: what is a higher order function? HOF's means we can pass functions as arguments to other functions
--- we also say functions are first-class citizens because we pass them around as if they are values
-
--- Why
--- haskells type system does not support first-class functions. this is not supported: function :: (Type -> Type) -> (Type)
--- we need this functionality because the map function is a higher order function: map :: (Type -> Type) -> [Type] -> [Type]
-
--- instead of passing type level functions around, we represent them as data
--- then we can apply them with type families
--- to achieve this, we need multi parameter typeclasses. ie typeclases using more than one type variable
-class Apply f a b where -- the typechecker checks that an instance of apply exists for the type variables before applying it.
+class Apply f a b where
   apply :: f -> a -> b
 
-data Valify = Valify -- this is known as a symbol function. The "ToValue" constructor symbolizes the function
+data Valify = Valify
 instance Apply Valify a (Value a) where
   apply _ x = Current (Just' x) x
 
-
--- The type family allows us to interpret the defunctionlizaed function by pattern matching on the data constructor to produce a result
--- This allows us to write higher order functions at the type level
-
-
--- Map takes a list of types
--- then we pattern match on the list of types, and map these to other types.
--- in this case, we map HList of with head of type Sig a to Value a recursively
 type family Map (xs :: [Type]) :: [Type] where
-  Map '[] = '[]
-  Map (Sig a ': xs) = Value a ': Map xs
-
--- Why do we need this type family? Because the return type of intermediate mappings is not clear from mapH
-
--- EXAMPLES
--- 1) normal map and list: map :: (a -> b) -> [a] -> [b]
--- map (\a -> if a == 0 then False else True) [1,2,3] = [True, False, False]
-
--- 2) using HList and mapH: mapH :: (Type -> Type) -> [Type] -> [Type]
--- map (\a -> if a == 0 then False else True) [1,False,"Hello"]
--- How is the compiler supposed to know the return type of mapH? After each function applicaiton, the return type changes
--- we need to guide the compiler to promise that for all list of types, there exists an instance of apply
-
+  Map '[] = '[] 
+  Map '[Value a] = '[Value a]
+  Map (x ': xs) = Value x ': Map xs
 
 class MapH f xs ys where
   mapH :: f -> HList xs -> HList ys
@@ -163,33 +109,20 @@ instance (Apply f x y, MapH f xs ys) => MapH f (x ': xs) (y ': ys) where
 instance (Apply f x (acc -> acc), FoldrH f acc xs) => FoldrH f acc (x ': xs) where
   foldrH f acc (HCons x xs) = apply f x $ foldrH f acc xs
 
-newtype WrappedMap xs = WrappedMap (HList (Map xs))
-
--- symbol function to collapse
 data Flatten = Flatten
-instance Apply Flatten (HList (Sig a ': xs)) (Sig (WrappedMap (Value a ': xs)) -> Sig (WrappedMap (Value a ': xs))) where
-  apply _ (HCons x xs) = prepend x
+instance (q ~ HList (Map xs), r ~ HList (Map xs)) => Apply Flatten (HList (Sig a ': xs)) (Sig q -> Sig r) where
+  apply _ (HCons x xs) = prepend x -- partially apply prepend, cps
 
+{-# ANN flattenToSignal' AllowRecursion #-}
+flattenToSignal' :: forall a xs. 
+  ( Stable a,
+    MapH    Valify  xs            (Map xs)
+    --FoldrH  Flatten xs            (Map (Value a) xs)
+  ) =>  HList (Sig a ': xs) -> Sig (HList (Map (Value a ': xs)))
+flattenToSignal' xs = foldrH Flatten (singleton' (HNil ::: never)) xs
 
-
-
--- {-# ANN flattenToSignal' AllowRecursion #-}
---  forall xs. (MapH ToValue xs (Map xs)) constraint
--- use forall as an existential type variable qualifier. ie. "there exists"
--- promise the compiler:
--- forall xs: when you apply ToValue xs, there exists a function from xs to another type
-
--- the constraint ensures xs can be mapped over
--- MapH f xs ys
--- MapH ToValue xs (Map xs), ie the return type ys is defined by (Map xs)
--- https://wasp.sh/blog/2021/09/01/haskell-forall-tutorial
--- https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/explicit_forall.html
--- flattenToSignal' :: forall a xs. 
---    (  MapH ToValue xs (Map xs),
---       FoldrH Flatten (Sig (HList (Map xs))) (Sig a ': xs),
---       Stable a
---    ) => HList (Sig a ': xs) -> Sig (HList (Map (Value a ': xs)))
--- flattenToSignal' xs = foldrH Flatten (singleton' (HNil ::: never)) xs
+-- We have tried to generalize to signals of any types by defining it in terms of fold
+-- We are currently struggling with trying to refac the function needed for the fold
 
 
 -- flattenToSignal :: (Stable a) => NonEmptyList (Sig a) -> Sig (HList (Value a))

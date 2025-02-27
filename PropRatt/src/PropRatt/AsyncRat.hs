fdccf1d328e7b2f093c068fd57c4da5d957eaf4b
MathiasFaber <makr20ah@student.cbs.dk>
2025-02-27 14:02:43 +0100

    reorder and cleanup

--- PropRatt/src/PropRatt/AsyncRat.hs
+++ PropRatt/src/PropRatt/AsyncRat.hs
@@ -13,7 +13,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 -- AsyncRattus code goes here. 
 -- The code is type checked by the AsyncRattus compiler plugin.
@@ -31,11 +30,19 @@ import AsyncRattus.InternalPrimitives
 import Prelude hiding (const, filter, getLine, map, null, putStrLn, zip, zipWith)
 import PropRatt.Value
 import Data.Kind (Type)
-import Test.QuickCheck (functionShow)
-import Unsafe.Coerce (unsafeCoerce)
 
 
--- data NonEmptyList a = NonEmptyList !a !(HList a)
+aRatZip :: Sig Int -> Sig Int -> Sig (Int :* Int)
+aRatZip a b = zip a b
+
+aRatSwitch :: Sig a -> O (Sig a) -> Sig a
+aRatSwitch a o = switch a o
+
+aRatParallel :: Sig a -> Sig b -> Sig (Maybe' a :* Maybe' b)
+aRatParallel a b = parallel a b
+
+
+
 
 -- strict HList
 data HList :: [Type] -> Type where
@@ -52,39 +59,6 @@ infixr 5 %:
 (%:) :: x -> HList xs -> HList (x ': xs)
 (%:) = HCons
 
-instance (Show x, (Show (HList xs))) => Show (HList (x ': xs)) where
-  show (HCons x xs) = show x ++ " %: " ++ show xs 
-
-aRatZip :: Sig Int -> Sig Int -> Sig (Int :* Int)
-aRatZip a b = zip a b
-
-aRatSwitch :: Sig a -> O (Sig a) -> Sig a
-aRatSwitch a o = switch a o
-
-aRatParallel :: Sig a -> Sig b -> Sig (Maybe' a :* Maybe' b)
-aRatParallel a b = parallel a b
-
-
--- -- We define an unsafe helper that “extends” HNil to any tail.
--- unsafeExtend :: forall xs. HList xs
--- unsafeExtend = unsafeCoerce HNil
-
-singleton' :: (Stable a) => Sig a -> Sig (HList (Map Value '[a])) -- Sig (HList (Map Value xs))
-singleton' xs = map (box (\p -> (Current (Just' p) p) %: HNil)) xs
-
-prepend :: (Stable a) => Sig a -> Sig (HList (Map Value xs)) -> Sig (HList (Map Value (a ': xs)))
-prepend (t ::: ts) (y ::: ys) = 
-  (HCons (Current (Just' t) t) y) ::: never --prependAwait xs ys
-
--- -- lacks maph constraint
--- -- lacks symbol function to map values to nothing
--- prependAwait :: (Stable a) => O (Sig (HList (Map (Value a ': xs)))) -> O (Sig (HList (Map (Value a ': xs)))) -> O (Sig (HList (Map (Value a ': xs))))
--- prependAwait xs ys = delay (
---   case select xs ys of
---      Fst (x' ::: xs')   ys'         -> (x' %: mapH (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait xs' ys'
---      Snd xs' (y' ::: ys')           -> mapH (\(Current _ x) -> Current Nothing' x) xs' %: y' ::: prependAwait xs' ys'
---      Both (x' ::: xs') (y' ::: ys') -> (x' %: y')  ::: prependAwait xs' ys')
-
 class Apply f a b where
   apply :: f -> a -> b
 
@@ -92,33 +66,24 @@ data Valify = Valify
 instance Apply Valify a (Value a) where
   apply _ x = Current (Just' x) x
 
--- type family Map  (xs :: [Type]) :: [Type] where
---   Map '[]       = '[]
---   Map (x ': xs) = Value x ': Map xs
+
+instance (Show x, (Show (HList xs))) => Show (HList (x ': xs)) where
+  show (HCons x xs) = show x ++ " %: " ++ show xs 
+
+class MapH f xs ys where
+  mapH :: f -> HList xs -> HList ys
+
+instance MapH f '[] '[] where
+   mapH _ _ = HNil
+
+instance (Apply f x y, MapH f xs ys) => MapH f (x ': xs) (y ': ys) where
+  mapH f (HCons x xs) = apply f x %: mapH f xs
+
 
 type family Map (f :: Type -> Type) (xs :: [Type]) :: [Type] where
   Map f '[] = '[]
   Map f (x ': xs) = f x ': Map f xs
 
-class MapH f xs ys where
-  mapH :: f -> HList xs -> HList ys
-
-class FoldrH f acc xs where
-  foldrH :: f -> acc -> HList xs -> acc
-  
-instance MapH f '[] '[] where
-   mapH _ _ = HNil
-
-instance FoldrH f acc '[] where
-   foldrH _ acc _ = acc
-
-instance (Apply f x y, MapH f xs ys) => MapH f (x ': xs) (y ': ys) where
-  mapH f (HCons x xs) = apply f x %: mapH f xs
-
-instance (Apply f x (acc -> acc), FoldrH f acc xs) => FoldrH f acc (x ': xs) where
-  foldrH f acc (HCons x xs) = apply f x $ foldrH f acc xs
-
--- type family AllStable (a ': as) 
 
 class AllStable (as :: [Type]) where 
 
@@ -127,27 +92,29 @@ instance AllStable '[] where
 instance (Stable a, AllStable as) => AllStable (a ': as) where
 
 
--- data Flatten = Flatten
--- instance (q ~ HList (Map xs), r ~ HList (Map xs)) => Apply Flatten (HList (Sig a ': xs)) (Sig q -> Sig r) where
---   apply _ (HCons x xs) = prepend x -- partially apply prepend, cps
 
--- --{-# ANN flattenToSignal AllowRecursion #-}
--- flattenToSignal :: AllStable as => HList (Map as) -> Sig (HList (Map as))
--- flattenToSignal xs = foldrH Flatten (singleton' (HNil ::: never)) xs
+singleton' :: (Stable a) => Sig a -> Sig (HList (Map Value '[a])) -- (Sig (HList (Map Value xs))) - Gives a type error
+singleton' xs = map (box (\p -> (Current (Just' p) p) %: HNil)) xs
 
--- flattenToSignal :: AllStable as => HList (Map Sig as) -> Sig (HList (Map Value as))
--- flattenToSignal (NonEmptyList h Nil) = singleton' h
--- flattenToSignal (NonEmptyList h t)   = prepend h (flattenToSignal' t)
-
-
--- Flatten as a recursive function instead of foldr
-flattenToSignal' :: (AllStable xs, AllStable as) => HList (Map Sig xs) -> Sig (HList (Map Value (a ': as)))
-flattenToSignal' (HCons head HNil) = singleton' head 
+-- Flatten as a recursive function
+flattenToSignal' :: AllStable as => HList (Map Sig as) -> Sig (HList (Map Value as))
+flattenToSignal' HNil = undefined
+flattenToSignal' (HCons head HNil) = singleton' head
 flattenToSignal' (HCons head tail) = prepend head (flattenToSignal' tail) 
 
-flattenToSignal :: (AllStable xs, AllStable as) => HList (Map Sig xs) -> Sig (HList (Map Value (a ': as)))
+-- Gives error: "could not deduce ‘x ~ Sig a3’ from the context: Map Sig xs ~ (x : xs4)" on "head" and "tail"
+flattenToSignal :: AllStable as => HList (Map Sig as) -> Sig (HList (Map Value as))
 flattenToSignal (HCons head tail) = prepend head (flattenToSignal' tail) 
 
+prepend :: (Stable a, AllStable as) => Sig a -> Sig (HList (Map Value as)) -> Sig (HList (Map Value (a ': as)))
+prepend (x ::: xs) (y ::: ys) = 
+  (HCons (Current (Just' x) x) y) ::: never --prependAwait x xs y ys
 
+-- Outcommented prependAwait. We need "flattenToSignal" and "prepend" to work first
 
-
+-- prependAwait :: (Stable a, AllStable as) => a -> O (Sig a) -> HList (Map Value as) -> O (Sig (HList (Map Value as))) -> O (Sig (HList (Map Value (a ': as))))
+-- prependAwait x xs y ys  = delay (
+--   case select xs ys of
+--      Fst (x' ::: xs')   ys'         -> (Current (Just' x') x' %: mapH (\(Current _ x) -> Current Nothing' x) y) ::: prependAwait x' xs' y ys'
+--      Snd xs' (y' ::: ys')           -> (Current Nothing' x %: y') ::: prependAwait x xs' y' ys'
+--      Both (x' ::: xs') (y' ::: ys') -> (Current (Just' x') x' %: y') ::: prependAwait x' xs' y' ys')
